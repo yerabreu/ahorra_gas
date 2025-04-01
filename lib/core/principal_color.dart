c94f079065001d962575a6d2cdd8bbbf0c1f09a1
yerabreu <yerayabreuglez@gmail.com>
2025-04-01 20:33:40 +0100

    Primer Modelo

---
+++ lib/core/principal_color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class ColorApp {
+  static const Color principalColor = Color(0xFF001d49);
+  static const Color letterColor = Color(0xFFFFFFFF);
+  static const Color colorButton = Color(0xFFFF6300); 
+}
